1d4ad95f433a3a618b1c5305748fc53a1b161d87
Keeksikook <49043011+Keeksikook@users.noreply.github.com>
2020-02-21 18:20:18 +0200

    Add files via upload

---
+++ MenuState.cpp
@@ -0,0 +1,51 @@
+#include "MenuState.h"
+
+MenuState::MenuState(sf::RenderWindow* window, sf::Event& event)
+	:
+	State(window, event, manager),
+	optionSelect({ "New Game", "About", "Exit" }, manager.font("Khand"), {480,260})
+{
+
+	backgroundSprite.setTexture(*manager.texture("MainMenuBackground"));
+	backgroundSprite.setScale(960.f / 1024.f, 540.f / 1024.f);
+}
+
+MenuState::~MenuState()
+{
+	endState();
+}
+
+void MenuState::endState()
+{
+}
+
+void MenuState::updateInput(const float& dt)
+{
+	checkQuit();
+}
+
+void MenuState::update(const float& dt)
+{
+	if (setting == Running)
+	{
+
+		updateInput(dt);
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::A))
+		{
+			optionSelect.next();
+		}
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::D))
+		{
+			optionSelect.prev();
+		}
+	}
+}
+
+void MenuState::draw(sf::RenderTarget* target)
+{
+	if (!target)
+		target = window;
+	target->draw(backgroundSprite);
+	optionSelect.draw(target);
+}
+
---
+++ OptionSelect.h
@@ -0,0 +1,35 @@
+#pragma once
+#include<SFML/Graphics.hpp>
+#include<vector>
+#include<iostream>
+
+class OptionSelect
+{
+	enum Behaviour {
+		Hover, Cursor, HoverCursor
+	};
+
+private:
+	Behaviour settings;
+	sf::Text* cursorPtr; 
+	sf::Vector2u position;
+	std::vector<sf::Text> options;
+	unsigned optionHeight;
+
+
+	//MODEL
+	sf::Text notSelected, selected;
+
+
+	void styleBefore();
+	void styleAfter();
+	void increment();
+	void decrement();
+public:
+	OptionSelect(std::vector<std::string> options, sf::Font* font, sf::Vector2u position, Behaviour settings = HoverCursor);
+	void draw(sf::RenderTarget* window);
+	void update();
+	void next();
+	void prev();
+	void select();
+};
